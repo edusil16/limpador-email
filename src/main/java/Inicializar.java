c47dcf13a16967c07c0a06a48c5d22b02e05921f
Eduardo Santos <edu.sil1606@gmail.com>
2020-02-13 16:54:46 -0300

    Update Inicializar.java

--- src/main/java/Inicializar.java
+++ src/main/java/Inicializar.java
@@ -6,8 +6,8 @@ public class Inicializar {
     public static void main(String[] args) throws IOException {
         // os argumentos nao servem para nada, ignoro
         String pastaLimpeza = "teste";
-        String userName = "sistema.tjrj.excel@mprj.mp.br";
-        String senha ="investigacao1";
+        String userName = "Email@teste.com.br";
+        String senha ="senha***";
         VisualizarMensagens visualizarMensagens = new VisualizarMensagens();
         MailExcluiMensagens mailExcluiMensagens = new MailExcluiMensagens();
 
